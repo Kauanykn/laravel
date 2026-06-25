4f049b481904a1c63ee06ea8df05b0117f3ebb82
Kauanykn <kauanyneves839@gmail.com>
2026-06-24 21:04:56 -0300

    validação

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -15,7 +15,28 @@ class AlunoController extends Controller
         return view('aluno.index', ['alunos'=>$aluno::all()]);
     }
 
+
+
     function add(Request $dados) { 
+     $validator = Validator::make(
+		      $dados->all(),
+	            [
+	                'nome' => 'required|min:3|max:255',
+	            ],
+	            [
+	                'nome.required' => 'O campo nome é obrigatório.',
+	                'nome.min' => 'O campo nome deve conter no mínimo 3 caracteres.',
+	                'nome.max' => 'O campo nome deve conter no máximo 255 caracteres.',
+	            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect()
+                ->route('aluno.index')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         $aluno = new \App\Models\AlunoModel();
         $aluno::create($dados->all());
 
