6925a94348ccd88d787fc2f2302d368fe0b4bdbd
kauanyKn <kauanyneves839@gmail.com>
2026-08-12 18:28:18 -0300

    12/08

--- database/migrations/2026_08_12_210913_create_contato_table.php
+++ database/migrations/2026_08_12_210913_create_contato_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('testes', function (Blueprint $table) {
+        Schema::create('contato', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nome', 100);
+            $table->string('email', 255);
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('testes');
+        Schema::dropIfExists('contato');
     }
 };
